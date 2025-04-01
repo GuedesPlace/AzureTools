27bc0c406fe93925a6a9ec5f03802d3a5903e584
Christian Güdemann <christian@guedesplace.ch>
2025-04-01 07:15:11 +0200

    Adding FunctionRunContext

---
+++ GuedesPlace.AzureTools/Functions/FunctionRunContext/IFunctionRunContext.cs
@@ -0,0 +1,14 @@
+namespace GuedesPlace.AzureTools.Functions.FunctionRunContext;
+public interface IFunctionRunContext {
+    public FunctionRunContextType FunctionRunContextType { get; }
+    public string? GetEnvironmentVariable(string name);
+    public ValueTask<T?> GetPayLoad<T>();
+    public string? GetUserId();
+    public string? GetUPN();
+    public bool IsAuthenticated();
+
+    public bool IsDev();
+
+    public bool IsInAtLeastOneRole(params string[] rolesToCheck);
+    public bool IsInAllRoles(params string[] rolesToCheck);
+}
